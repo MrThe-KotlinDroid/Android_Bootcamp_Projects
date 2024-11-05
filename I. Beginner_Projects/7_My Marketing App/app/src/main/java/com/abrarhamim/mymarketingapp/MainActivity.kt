7c5522473cbe4b93d1bd57053dea0d45ba02a745
Abrar <abrarhamim2@gmail.com>
2024-11-05 16:35:45 +0600

    Updated

--- I. Beginner_Projects/7_My Marketing App/app/src/main/java/com/abrarhamim/mymarketingapp/MainActivity.kt
+++ I. Beginner_Projects/7_My Marketing App/app/src/main/java/com/abrarhamim/mymarketingapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.abrarhamim.mymarketingapp
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.Spinner
