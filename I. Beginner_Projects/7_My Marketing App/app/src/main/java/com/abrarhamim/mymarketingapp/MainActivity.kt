2d5b4d33e992e2e03a6d2afb6c117cfb0bc41e14
SahilplaySoftware <abrarhamim2@gmail.com>
2024-11-10 03:34:58 +0600

    Code push with new learning of Kotlinx Extension

--- I. Beginner_Projects/7_My Marketing App/app/src/main/java/com/abrarhamim/mymarketingapp/MainActivity.kt
+++ I. Beginner_Projects/7_My Marketing App/app/src/main/java/com/abrarhamim/mymarketingapp/MainActivity.kt
@@ -6,37 +6,36 @@ import android.widget.CheckBox
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
 import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.button_preview
+import kotlinx.android.synthetic.main.activity_main.check_box_immediate_start
+import kotlinx.android.synthetic.main.activity_main.check_box_junior
+import kotlinx.android.synthetic.main.activity_main.edit_text_contact_name
+import kotlinx.android.synthetic.main.activity_main.edit_text_contact_number
+import kotlinx.android.synthetic.main.activity_main.edit_text_my_display_name
+import kotlinx.android.synthetic.main.activity_main.edit_text_start_date
+import kotlinx.android.synthetic.main.activity_main.spinner_job_title
 
 class MainActivity : AppCompatActivity() {
 
-    private var contactNameEditText: TextInputEditText? = null
-    private var contactNumberEditText: TextInputEditText? = null
-    private var myDisplayNameEditText: TextInputEditText? = null
-    private var startDateEditText: TextInputEditText? = null
-    private var juniorCheckBox: CheckBox? = null
-    private var immediateStartCheckBox: CheckBox? = null
-    private var jobTitleSpinner: Spinner? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        contactNameEditText = findViewById(R.id.edit_text_contact_name)
-        contactNumberEditText = findViewById(R.id.edit_text_contact_number)
-        myDisplayNameEditText = findViewById(R.id.edit_text_my_display_name)
-        startDateEditText = findViewById(R.id.edit_text_start_date)
-        juniorCheckBox = findViewById(R.id.check_box_junior)
-        immediateStartCheckBox = findViewById(R.id.check_box_immediate_start)
-        jobTitleSpinner = findViewById(R.id.spinner_job_title)
 
-        val previewButton: Button = findViewById(R.id.button_preview)
-        previewButton.setOnClickListener {
+        button_preview.setOnClickListener {
             onPreviewClicked()
         }
     }
 
     private fun onPreviewClicked() {
-        val testString = contactNameEditText!!.text.toString() + ", " + contactNumberEditText!!.text.toString()
-        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
+        val contactName = edit_text_contact_name.text.toString()
+        val contactNumber = edit_text_contact_number.text.toString()
+        val myDisplayName = edit_text_my_display_name.text.toString()
+        val includeJunior = check_box_junior.isChecked
+        val jobDescription = spinner_job_title.selectedItem.toString()
+        val immediateStart = check_box_immediate_start.isChecked
+        val startDate = edit_text_start_date.text.toString()
     }
 }
