f52d71a2c6ebf342830a5e1f775208a97dee23ed
SahilplaySoftware <abrarhamim2@gmail.com>
2024-12-12 22:42:03 +0600

    git push

--- I. Beginner_Projects/6_My Bucket List App/app/src/main/java/com/abrarhamim/mybucketlistapp/MyBucketListHome.java
+++ I. Beginner_Projects/6_My Bucket List App/app/src/main/java/com/abrarhamim/mybucketlistapp/MyBucketListHome.java
@@ -30,7 +30,6 @@ public class MyBucketListHome extends AppCompatActivity {
             }
         });
 
-
         placesToGoCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
