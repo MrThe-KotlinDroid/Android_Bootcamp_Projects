204a3f57493121c14022a26836bf2398468c4aa3
SahilplaySoftware <abrarhamim2@gmail.com>
2024-12-22 02:24:25 +0600

    Null Safety Learned

--- I. Beginner_Projects/7_The Sameless Self Promotion App/app/src/main/java/com/abrar/a7_thesamelessselfpromotionapp/MainActivity.kt
+++ I. Beginner_Projects/7_The Sameless Self Promotion App/app/src/main/java/com/abrar/a7_thesamelessselfpromotionapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.abrar.a7_thesamelessselfpromotionapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,24 +11,34 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 
+private var contactNameEditText: TextInputEditText? = null
+private var contactNumberEditText: TextInputEditText? = null
+private var myDisplayNameEditText: TextInputEditText? = null
+private var startDateEditText: TextInputEditText? = null
+private var juniorCheckBox: CheckBox? = null
+private var immediateStartCheckBox: CheckBox? = null
+private var jobTitleSpinner: Spinner? = null
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        contactNameEditText = findViewById(R.id.edit_text_contact_name);
+        contactNumberEditText = findViewById(R.id.edit_text_contact_number);
+        myDisplayNameEditText = findViewById(R.id.edit_text_display_name);
+        startDateEditText = findViewById(R.id.edit_text_start_date);
+        juniorCheckBox = findViewById(R.id.check_box_include_junior);
+        immediateStartCheckBox = findViewById(R.id.check_box_immediate_start);
+        jobTitleSpinner = findViewById(R.id.spinner_job_title);
+
         setContentView(R.layout.activity_main)
         val previewButton: Button = findViewById(R.id.button_preview)
         previewButton.setOnClickListener {
             onPreviewClicked()
-
         }
     }
 
-
     private fun onPreviewClicked() {
-        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name)
-        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number)
-
-        val testString = contactNameEditText.text.toString() + ", " + contactNumberEditText.text.toString()
+        val testString = contactNameEditText?.text.toString() + ", " + contactNumberEditText?.text.toString()
         Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
     }
 }
