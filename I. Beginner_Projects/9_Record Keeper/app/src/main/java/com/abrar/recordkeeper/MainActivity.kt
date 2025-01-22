836b49ca745c3f17c446cf7d888fb49830418cb5
SahilplaySoftware <abrarhamim2@gmail.com>
2025-01-23 00:27:07 +0600

    updated for fragments

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/MainActivity.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.abrar.recordkeeper
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.abrar.recordkeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNav
+        binding.frameContent
     }
-
 }
