63f061b85ee3b3000f235ac2f8d523e6b6238c79
SahilplaySoftware <abrarhamim2@gmail.com>
2025-01-24 18:19:16 +0600

    Bottom navigation

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/MainActivity.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.abrar.recordkeeper
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.abrar.recordkeeper.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         binding.buttonCycling.setOnClickListener { onCyclingClicked() }
         binding.buttonRunning.setOnClickListener { onRunningClicked() }
@@ -32,4 +34,5 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.frame_content, CyclingFragment())
         }
     }
+
 }
