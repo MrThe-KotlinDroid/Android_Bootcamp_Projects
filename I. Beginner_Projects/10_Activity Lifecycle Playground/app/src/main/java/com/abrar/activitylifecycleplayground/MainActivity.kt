f65af8dddb39b363df79f51f0d0b82e737d6681c
SahilplaySoftware <abrarhamim2@gmail.com>
2025-02-03 15:05:16 +0600

    last updated

--- I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/MainActivity.kt
+++ I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.abrar.activitylifecycleplayground
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        findViewById<Button>(R.id.button_exit).setOnClickListener {
+            Log.d("Abrar Hamim", "In the button click listener!, Exiting the app!")
+            finish()
+        }
+        Log.d("Abrar Hamim", "I'm in onCreate()!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Abrar Hamim", "I'm now in onStart()!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Abrar Hamim", "I'm now in onResume()!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Abrar Hamim", "I'm now in onPause()!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Abrar Hamim", "I'm now in onStop()!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Abrar Hamim", "I'm now in onDestroy()!")
     }
 }
