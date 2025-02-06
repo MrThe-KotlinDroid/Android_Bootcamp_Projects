3a9704bd98e8e50160564bada8471965d5edae14
SahilplaySoftware <abrarhamim2@gmail.com>
2025-02-06 21:44:51 +0600

    Activity lifecycle learning

--- I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/MainActivity.kt
+++ I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/MainActivity.kt
@@ -7,51 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.abrar.activitylifecycleplayground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        findViewById<Button>(R.id.button_exit).setOnClickListener {
-            Log.d("Abrar Hamim", "In the button click listener!, Exiting the app!")
+        binding.buttonExit.setOnClickListener {
             finish()
         }
-        Log.d("Abrar Hamim", "I'm in onCreate()!")
-    }
-
-    // When our Activity becomes visible to the user
-    override fun onStart() {
-        super.onStart()
-        Log.d("Abrar Hamim", "I'm now in onStart()!")
-    }
-
-    // When our Activity is in the foreground and the user can interact with it
-    override fun onResume() {
-        super.onResume()
-        Log.d("Abrar Hamim", "I'm now in onResume()!")
-    }
-
-    // When our Activity is still visible, but it is in the background
-    // (e.g. An Activity "Dialog" is on top, but we can still see our Activity
-    override fun onPause() {
-        super.onPause()
-        Log.d("Abrar Hamim", "I'm now in onPause()!")
-    }
-
-    // When our Activity is no longer visible (but it's running)
-    override fun onStop() {
-        super.onStop()
-        Log.d("Abrar Hamim", "I'm now in onStop()!")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("Abrar Hamim", "I'm now in onDestroy()!")
     }
 }
