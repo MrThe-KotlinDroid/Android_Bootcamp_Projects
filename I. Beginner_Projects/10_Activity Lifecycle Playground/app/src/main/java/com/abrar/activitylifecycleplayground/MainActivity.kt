ea3c1ec44582fdb2f2372bed61e87cbc4298b28f
SahilplaySoftware <abrarhamim2@gmail.com>
2025-02-08 23:57:14 +0600

    git update

--- I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/MainActivity.kt
+++ I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/MainActivity.kt
@@ -34,5 +34,4 @@ class MainActivity : AppCompatActivity() {
         val userMessage = binding.editTextMessage.text
         File(filesDir, "user message.txt").writeText(userMessage.toString())
     }
-
 }
