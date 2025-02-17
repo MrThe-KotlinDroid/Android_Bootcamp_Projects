625ee5938229320d8d7cdcf1c6c3fc25bea084d2
Abrar Hamim <abrarhamim2@gmail.com>
2025-02-18 03:49:25 +0600

    git update

--- I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/MainActivity.kt
+++ I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
 //        File(filesDir, "user message.txt").writeText(userMessage.toString())
 //    }
 
+
     override fun onBackPressed() {
         super.onBackPressed()
         Toast.makeText(this, "Back button pressed", Toast.LENGTH_SHORT).show()
