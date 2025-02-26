1b14d95e6e2e33d1c70f09929547aca15ba9cfe9
Abrar Hamim <abrarhamim2@gmail.com>
2025-02-26 12:51:17 +0600

    add TestFragmentListener interface and implement fragment listener in TestFragment

--- I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/TestFragment.kt
+++ I. Beginner_Projects/10_Activity Lifecycle Playground/app/src/main/java/com/abrar/activitylifecycleplayground/TestFragment.kt
@@ -1,5 +1,6 @@
 package com.abrar.activitylifecycleplayground
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,12 @@ import com.abrar.activitylifecycleplayground.databinding.FragmentTestBinding
 class TestFragment: Fragment() {
 
     private lateinit var binding: FragmentTestBinding
+    private var fragmentListener: TestFragmentListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        fragmentListener = context as TestFragmentListener
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +30,20 @@ class TestFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.textViewFragmentText.text = "We have made it to the Fragment. This is our onViewCreated() code."
+        binding.textViewFragmentText.setOnClickListener {
+            fragmentListener?.clearActivityScreen()
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        fragmentListener = null
+    }
+
+    interface TestFragmentListener {
+
+        fun clearActivityScreen()
+
     }
 
 }
