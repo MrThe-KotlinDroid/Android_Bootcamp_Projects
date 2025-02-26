74662e42fdbfcb7eac480fa66562e0bed7ced290
Abrar Hamim <abrarhamim2@gmail.com>
2025-02-26 23:42:29 +0600

    rename RunningFragment package and update references in MainActivity and layout

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/MainActivity.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.abrar.recordkeeper.databinding.ActivityMainBinding
+import com.abrar.recordkeeper.running.RunningFragment
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/RunningFragment.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/RunningFragment.kt
@@ -1,4 +1,4 @@
-package com.abrar.recordkeeper
+package com.abrar.recordkeeper.running
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.abrar.recordkeeper.EditRunningRecordActivity
 import com.abrar.recordkeeper.databinding.FragmentRunningBinding
 
 class RunningFragment : Fragment() {
