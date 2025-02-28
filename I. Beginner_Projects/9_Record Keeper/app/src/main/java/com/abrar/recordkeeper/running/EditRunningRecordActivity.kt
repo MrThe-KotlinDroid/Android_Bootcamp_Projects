edd4070f0b87e827f254c344d47da8893c0b1ce9
Abrar Hamim <abrarhamim2@gmail.com>
2025-02-28 18:10:27 +0600

    feat: implement record saving functionality in EditRunningRecordActivity

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/EditRunningRecordActivity.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/EditRunningRecordActivity.kt
@@ -1,7 +1,9 @@
 package com.abrar.recordkeeper.running
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import com.abrar.recordkeeper.databinding.ActivityEditRunningRecordBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
@@ -15,5 +17,22 @@ class EditRunningRecordActivity : AppCompatActivity() {
         val distance = intent.getStringExtra("Distance")
         title = "$distance Record"
 
+
+
+        binding.buttonSave.setOnClickListener { saveRecord(distance) }
+    }
+
+    private fun saveRecord(distance: String?) {
+        val record = binding.editTextRecord.text.toString()
+        val date = binding.editTextDate.text.toString()
+
+        val runningPreferences = getSharedPreferences("RunningRecords", MODE_PRIVATE)
+
+        runningPreferences.edit {
+            putString("$distance record", record)
+            putString("$distance date", date)
+
+            Toast.makeText(this@EditRunningRecordActivity, "Record saved", Toast.LENGTH_SHORT).show()
+        }
     }
 }
