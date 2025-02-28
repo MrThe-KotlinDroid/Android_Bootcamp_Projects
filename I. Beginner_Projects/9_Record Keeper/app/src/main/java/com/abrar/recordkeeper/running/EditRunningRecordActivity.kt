69946d040744ccd6e3e4cb1ceca1448b8a1bae06
Abrar Hamim <abrarhamim2@gmail.com>
2025-02-28 17:29:49 +0600

    feat: add additional shared preferences management in EditRunningRecordActivity

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/EditRunningRecordActivity.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/EditRunningRecordActivity.kt
@@ -1,5 +1,6 @@
 package com.abrar.recordkeeper.running
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
@@ -22,5 +23,17 @@ class EditRunningRecordActivity : AppCompatActivity() {
         applicationPreferences.edit {
             putString("Some application data", "Application preference value here")
         }
+
+        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+
+        activityPreferences.edit {
+            putInt("Some activity data", 15)
+        }
+
+        val fileNamePreferences = getSharedPreferences("Some shared preferences file", Context.MODE_PRIVATE)
+        fileNamePreferences.edit {
+            putBoolean("Some preference file name data", false)
+        }
+
     }
 }
