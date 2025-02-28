dad15474375c312febd5c30e9a58c439b905838d
Abrar Hamim <abrarhamim2@gmail.com>
2025-02-28 17:38:49 +0600

    refactor: remove unused shared preferences initialization in EditRunningRecordActivity

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/EditRunningRecordActivity.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/EditRunningRecordActivity.kt
@@ -1,10 +1,7 @@
 package com.abrar.recordkeeper.running
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.edit
-import androidx.preference.PreferenceManager
 import com.abrar.recordkeeper.databinding.ActivityEditRunningRecordBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
@@ -17,24 +14,6 @@ class EditRunningRecordActivity : AppCompatActivity() {
         setContentView(binding.root)
         val distance = intent.getStringExtra("Distance")
         title = "$distance Record"
-
-        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-
-        applicationPreferences.edit {
-            putString("Some application data", "Application preference value here")
-        }
-
-        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
-
-        activityPreferences.edit {
-            putInt("Some activity data", 15)
-        }
-
-
-        val fileNamePreferences = getSharedPreferences("Some shared preferences file", Context.MODE_PRIVATE)
-        fileNamePreferences.edit {
-            putBoolean("Some preference file name data", false)
-        }
 
     }
 }
