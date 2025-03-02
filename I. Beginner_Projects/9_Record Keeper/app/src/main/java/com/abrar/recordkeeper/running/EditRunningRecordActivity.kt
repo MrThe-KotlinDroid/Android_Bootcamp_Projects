c3f0d4e279d0993536f27ee973580e899212808b
Abrar Hamim <abrarhamim2@gmail.com>
2025-03-02 21:05:44 +0600

    refactor: simplify SharedPreferences initialization in EditRunningRecordActivity

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/EditRunningRecordActivity.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/running/EditRunningRecordActivity.kt
@@ -1,6 +1,5 @@
 package com.abrar.recordkeeper.running
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,14 +9,13 @@ import com.abrar.recordkeeper.databinding.ActivityEditRunningRecordBinding
 class EditRunningRecordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditRunningRecordBinding
-    private lateinit var runningPreferences: SharedPreferences
+    private val runningPreferences by lazy { getSharedPreferences("RunningRecords", MODE_PRIVATE)}
     private val distance: String? by lazy { intent.getStringExtra("Distance") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditRunningRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        runningPreferences = getSharedPreferences("RunningRecords", MODE_PRIVATE)
 
         displayRecord()
         binding.buttonSave.setOnClickListener {
