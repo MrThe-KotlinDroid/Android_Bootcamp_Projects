6ef63b2b6a91c50ae9113b8ac3d7102c73757bbd
Abrar Hamim <abrarhamim2@gmail.com>
2025-03-05 16:47:07 +0600

    refactor: update launchCyclingRecordScreen to use EditRecordActivity and include record field hints

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/cycling/CyclingFragment.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/cycling/CyclingFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.abrar.recordkeeper.databinding.FragmentCyclingBinding
+import com.abrar.recordkeeper.editrecord.EditRecordActivity
 
 class CyclingFragment: Fragment() {
 
@@ -27,14 +28,14 @@ class CyclingFragment: Fragment() {
     }
 
     private fun setUpClickListeners() {
-        binding.containerLongestRide.setOnClickListener { launchCyclingRecordScreen("Longest Ride") }
-        binding.containerBiggestClimb.setOnClickListener { launchCyclingRecordScreen("Biggest Climb") }
-        binding.containerBestAverageSpeed.setOnClickListener { launchCyclingRecordScreen("Best Average Speed") }
+        binding.containerLongestRide.setOnClickListener { launchCyclingRecordScreen("Longest Ride", "Distance") }
+        binding.containerBiggestClimb.setOnClickListener { launchCyclingRecordScreen("Biggest Climb", "Height") }
+        binding.containerBestAverageSpeed.setOnClickListener { launchCyclingRecordScreen("Best Average Speed", "Average Speed") }
     }
 
-    private fun launchCyclingRecordScreen(record: String) {
-        val intent = Intent(context, EditCyclingRecordActivity::class.java)
-        intent.putExtra("Record", record)
+    private fun launchCyclingRecordScreen(record: String, recordFieldHint: String) {
+        val intent = Intent(context, EditRecordActivity::class.java)
+        intent.putExtra("screen_data", EditRecordActivity.ScreenData(record, "cycling", recordFieldHint))
         startActivity(intent)
     }
 }
