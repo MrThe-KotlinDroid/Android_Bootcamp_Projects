05220c9fab6b9c1e511b292b174b4f61caebcb28
Abrar Hamim <abrarhamim2@gmail.com>
2025-03-23 04:48:54 +0600

    Refactor MainActivity to improve code readability and consistency

--- I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/MainActivity.kt
+++ I. Beginner_Projects/9_Record Keeper/app/src/main/java/com/abrar/recordkeeper/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -58,7 +59,6 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
         return menuClickHandled
     }
-
 
     private fun showConfirmationDialog(selection: String) {
         AlertDialog.Builder(this)
