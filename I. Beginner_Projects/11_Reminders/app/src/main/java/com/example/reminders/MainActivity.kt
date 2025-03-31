deabb08b5231aab6b3b0a61ac698805bddae761f
Abrar Hamim <abrarhamim2@gmail.com>
2025-03-31 22:12:58 +0600

    Updated several things

---
+++ I. Beginner_Projects/11_Reminders/app/src/main/java/com/example/reminders/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.reminders
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.reminders.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.viewPager.adapter = ScreenSlidePagerAdapter(this)
+    }
+
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = 2
+
+        override fun createFragment(position: Int) = when (position) {
+            0 -> PasswordsFragment()
+            else -> GeneralInfoFragment()
+        }
+    }
+}
