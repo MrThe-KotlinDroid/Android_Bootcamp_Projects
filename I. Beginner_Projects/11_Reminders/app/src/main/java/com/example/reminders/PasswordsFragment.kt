b2c464cf4652042446dd8faa441c5267d96703ce
Abrar Hamim <abrarhamim2@gmail.com>
2025-04-08 20:03:36 +0600

    feat: implement edit dialog for WiFi, Tablet Pin, and Bike Lock preferences; update styles and themes

--- I. Beginner_Projects/11_Reminders/app/src/main/java/com/example/reminders/PasswordsFragment.kt
+++ I. Beginner_Projects/11_Reminders/app/src/main/java/com/example/reminders/PasswordsFragment.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
+import com.example.reminders.databinding.DialogEditReminderBinding
 import com.example.reminders.databinding.FragmentPasswordsBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class PasswordsFragment: Fragment() {
 
@@ -27,11 +30,35 @@ class PasswordsFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         displayValues()
+        binding.cardViewWifi.setOnClickListener { showEditDialog(PREF_WIFI) }
+        binding.cardViewTabletPin.setOnClickListener { showEditDialog(PREF_TABLET_PIN) }
+        binding.cardViewBikeLock.setOnClickListener { showEditDialog(PREF_BIKE_LOCK) }
     }
 
     private fun displayValues() {
-        binding.textViewWifiValue.text = preferences.getString("pref_wifi", null)
-        binding.textViewTabletPinValue.text = preferences.getString("pref_tablet_pin", null)
-        binding.textViewBikeLockValue.text = preferences.getString("pref_bike_lock", null)
+        binding.textViewWifiValue.text = preferences.getString(PREF_WIFI, null)
+        binding.textViewTabletPinValue.text = preferences.getString(PREF_TABLET_PIN, null)
+        binding.textViewBikeLockValue.text = preferences.getString(PREF_BIKE_LOCK, null)
+    }
+
+    private fun showEditDialog(preferenceKey: String) {
+        val dialogBinding = DialogEditReminderBinding.inflate(LayoutInflater.from(requireContext()))
+        dialogBinding.editTextValue.setText(preferences.getString(preferenceKey, null))
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Update Value")
+            .setView(dialogBinding.root)
+            .setPositiveButton("save") { _, _ ->
+                preferences.edit { putString(preferenceKey, dialogBinding.editTextValue.text?.toString()) }
+                displayValues()
+            }
+            .setNegativeButton("cancel") { dialog , _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    companion object {
+        const val PREF_WIFI = "pref_wifi"
+        const val PREF_TABLET_PIN = "pref_tablet_pin"
+        const val PREF_BIKE_LOCK = "pref_bike_lock"
     }
 }
