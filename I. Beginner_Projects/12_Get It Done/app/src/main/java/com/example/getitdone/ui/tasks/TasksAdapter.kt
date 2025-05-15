7408813f105d384e4227febeec420b76864ff40a
Abrar Hamim <abrarhamim2@gmail.com>
2025-05-15 22:24:18 +0600

    feat: Implement TasksAdapter for displaying task items in RecyclerView

--- I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/ui/tasks/TasksAdapter.kt
+++ I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/ui/tasks/TasksAdapter.kt
@@ -1,5 +1,33 @@
 package com.example.getitdone.ui.tasks
 
-class TasksAdapter {
-    
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.getitdone.data.Task
+import com.example.getitdone.databinding.ItemTaskBinding
+
+class TasksAdapter(private val tasks: List<Task>) :
+    RecyclerView.Adapter<TasksAdapter.ViewHolder>() {
+
+    override fun getItemCount() = tasks.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(ItemTaskBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(tasks[position])
+    }
+
+    inner class ViewHolder(private val binding: ItemTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(task: Task) {
+            binding.textViewTitle.text = task.title
+            binding.textViewDetails.text = task.description
+        }
+
+    }
+
 }
