de7109d5e3d84b028f158295df7419106746e625
Abrar Hamim <abrarhamim2@gmail.com>
2025-05-16 20:05:05 +0600

    feat: Implement singleton pattern for database instance in GetItDoneDatabase

--- I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/data/GetItDoneDatabase.kt
+++ I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/data/GetItDoneDatabase.kt
@@ -8,15 +8,23 @@ import androidx.room.RoomDatabase
 @Database(entities = [Task::class], version = 1)
 abstract class GetItDoneDatabase : RoomDatabase() {
 
-    abstract fun getTaskDao() : TaskDao
+    abstract fun getTaskDao(): TaskDao
 
     companion object {
+
+        private var DATABASE_INSTANCE: GetItDoneDatabase? = null
+
         fun createDatabase(context: Context): GetItDoneDatabase {
-            return Room.databaseBuilder(
-                context,
-                GetItDoneDatabase::class.java,
-                "get-it-done-database"
-            ).build()
+            // If (we don't have a database) make one! else return the existing one
+            return DATABASE_INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    GetItDoneDatabase::class.java,
+                    "get-it-done-databse"
+                ).build()
+                DATABASE_INSTANCE = instance
+                instance
+            }
         }
     }
 }
