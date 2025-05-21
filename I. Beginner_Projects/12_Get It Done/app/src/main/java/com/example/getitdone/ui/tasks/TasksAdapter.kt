1a2e4bfe6040852cf705359b9fe245dbeb3eca75
Abrar Hamim <abrarhamim2@gmail.com>
2025-05-21 21:21:12 +0600

    feat: Update TasksAdapter to reflect task completion and starring status

--- I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/ui/tasks/TasksAdapter.kt
+++ I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/ui/tasks/TasksAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.getitdone.ui.tasks
 
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -25,6 +26,12 @@ class TasksAdapter(private val tasks: List<Task>, private val listener: TaskUpda
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(task: Task) {
+            binding.checkBox.isChecked = task.isComplete
+            binding.toggleStar.isChecked = task.isStarred
+            if (task.isComplete) {
+                binding.textViewTitle.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                binding.textViewDetails.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            }
             binding.textViewTitle.text = task.title
             binding.textViewDetails.text = task.description
             binding.checkBox.addOnCheckedStateChangedListener { _, state ->
