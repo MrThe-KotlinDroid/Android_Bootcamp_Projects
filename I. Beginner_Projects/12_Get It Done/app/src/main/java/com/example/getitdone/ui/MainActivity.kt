a9787c704299881da69fce7e1643ab2f08cead6e
Abrar Hamim <abrarhamim2@gmail.com>
2025-06-17 21:03:56 +0600

    refactor: Add input validation for task title in MainActivity

--- I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/ui/MainActivity.kt
+++ I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/ui/MainActivity.kt
@@ -57,6 +57,7 @@ class MainActivity : AppCompatActivity() {
                     if (editTextTaskDetails.isVisible) View.GONE else View.VISIBLE
             }
 
+
             buttonSave.setOnClickListener {
                 val task = Task(
                     title = editTextTaskTitle.text.toString(),
