dc328a4d811617a19e9faa8191c54d3d3e3610a7
Abrar Hamim <abrarhamim2@gmail.com>
2025-06-26 17:27:16 +0600

    chore: Update compileSdk to 36 and adjust input validation logic with new tests

--- I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/util/InputValidator.kt
+++ I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/util/InputValidator.kt
@@ -3,6 +3,6 @@ package com.example.getitdone.util
 object InputValidator {
 
     fun isInputValid(input: String?): Boolean {
-        return !input?.trim().isNullOrEmpty() && input!!.length > 1
+        return !input?.trim().isNullOrEmpty() && input.length > 1
     }
 }
--- I. Beginner_Projects/12_Get It Done/app/src/test/java/com/example/getitdone/InputValidationTest.kt
+++ I. Beginner_Projects/12_Get It Done/app/src/test/java/com/example/getitdone/InputValidationTest.kt
@@ -1,8 +1,8 @@
 package com.example.getitdone
 
-import com.example.getitdone.ui.MainActivity
 import com.example.getitdone.util.InputValidator
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class InputValidationTest {
@@ -11,6 +11,42 @@ class InputValidationTest {
     fun inputValidator_returnsFalseWhenEmpty() {
         // Perform an action
         val result = InputValidator.isInputValid("")
+
+        // Assert the result
+        assertFalse(result)
+    }
+
+    @Test
+    fun inputValidator_returnsFalseWhenNull() {
+        // Perform an action
+        val result = InputValidator.isInputValid(null)
+
+        // Assert the result
+        assertFalse(result)
+    }
+
+    @Test
+    fun inputValidator_returnsFalseWhenOnlyWhiteSpace() {
+        // Perform an action
+        val result = InputValidator.isInputValid("      ")
+
+        // Assert the result
+        assertFalse(result)
+    }
+
+    @Test
+    fun inputValidator_returnsTrueWhenMoreThanOneNonWhiteSpaceCharacter() {
+        // Perform an action
+        val result = InputValidator.isInputValid("more than one character")
+
+        // Assert the result
+        assertTrue(result)
+    }
+
+    @Test
+    fun inputValidator_returnsFalseWhenOnlyNonWhiteSpaceCharacter() {
+        // Perform an action
+        val result = InputValidator.isInputValid("a")
 
         // Assert the result
         assertFalse(result)
