b03e813d1c53d0978b8cd44418c8a017ad34e345
abrar_hamim <abrarhamim2@gmail.com>
2025-09-16 22:56:26 +0600

    chore: Specify table name for Task entity

--- I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/data/model/Task.kt
+++ I. Beginner_Projects/12_Get It Done/app/src/main/java/com/example/getitdone/data/model/Task.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "task")
 data class Task(
     @ColumnInfo(name = "task_id") @PrimaryKey(autoGenerate = true) val taskId: Int = 0,
     val title: String,
